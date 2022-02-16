3ae99ceba0a48e53ff2913772962237fd4d6fe72
vkpatil <vishalvkpatil8@gmail.com>
2022-02-16 20:24:31 +0530

    [vishal]Added: code for checking paramete validations

--- employeePayrollApp.py
+++ employeePayrollApp.py
@@ -2,15 +2,20 @@
 @Author: Vishal Patil
 @Date: 16-02-2022 11-00-00
 @Last Modified by: Vishal Patil
-@Last Modified time: 16-02-2022 11:30:00
+@Last Modified time: 16-02-2022 20:30:00
 @Title : create app for Employee Payroll App
 """
-from fastapi import FastAPI
-from pydantic import BaseModel
+import pydantic
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel, validator
 from typing import Optional
 from datetime import datetime
 
 app = FastAPI()
+
+
+class GenderError(Exception):
+    pass
 
 
 class User(BaseModel):
@@ -21,17 +26,53 @@ class User(BaseModel):
     start_date: Optional[datetime] = None
     notes: Optional[str] = None
 
+    @validator("gender")
+    @classmethod
+    def gender_valid(cls, gender):
+        """
+        Description: Showing Welcome msg
+        :type gender: Gender input for checking
+        Return: gender object or http exception
+        """
+        char = ["Male", "Female"]
+        if gender in char:
+            return gender
+        raise HTTPException(status_code=422, detail="Enter valid gender")
+
+    @validator("department")
+    @classmethod
+    def gender_valid(cls, department):
+        """
+        Description: Showing Welcome msg
+        :type department: department input for checking
+        Return: department object or http exception
+        """
+        char = ["HR", "Sales", "Finance", "Engineer", "Others"]
+        if department in char:
+            return department
+        raise HTTPException(status_code=422, detail="Enter valid Department")
+
 
 inventory = {}
 
 
 @app.get("/")
 def home():
+    """
+    Description: Showing Welcome msg
+    Return: Welcome msg
+    """
     return {"Welcome in Employee Payroll App"}
 
 
 @app.post("/create-user/{user_id}")
 def create_user(user_id: int, user: User):
+    """
+    Description: Creating user according to give data
+    :param user: creating object of user
+    :type user_id: giving user id
+    Return: string
+    """
     if user_id in inventory:
         return {"Error": "Item Id already exists "}
     inventory[user_id] = {"Name": user.name, "Gender": user.gender, "Department": user.department,
@@ -41,11 +82,21 @@ def create_user(user_id: int, user: User):
 
 @app.get("/dashboard")
 def dashboard():
+    """
+    Description: Showing Users data
+    Return: Dictionary
+    """
     return inventory
 
 
-@app.put("/update-item/{user_id}")
+@app.put("/update-user/{user_id}")
 def update_user(user_id: int, user: User):
+    """
+    Description: Updating user data
+    :param user: accessing object of user
+    :type user_id: giving user id
+    Return: string
+    """
     if user_id not in inventory:
         return {"Error": "Not Found"}
     if user.name is not None:
@@ -61,4 +112,12 @@ def update_user(user_id: int, user: User):
     if user.notes is not None:
         inventory[user_id]["Notes"] = user.notes
 
+    return {f"User update with name as {user.name}"}
+
+
+@app.delete("/delete-user")
+def delete_user(item_id: int, user: User):
+    if item_id not in inventory:
+        return {"Error": "Not available this id"}
+    del inventory[item_id]
     return {f"User update with name as {user.name}"}
