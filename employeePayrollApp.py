39791d8875b9ec4c5efeeb2bcc5f1befa7fa6cbb
vkpatil <vishalvkpatil8@gmail.com>
2022-02-16 11:30:59 +0530

    [vishal]Added: code for update user

--- employeePayrollApp.py
+++ employeePayrollApp.py
@@ -2,7 +2,7 @@
 @Author: Vishal Patil
 @Date: 16-02-2022 11-00-00
 @Last Modified by: Vishal Patil
-@Last Modified time: 16-02-2022 11:20:00
+@Last Modified time: 16-02-2022 11:30:00
 @Title : create app for Employee Payroll App
 """
 from fastapi import FastAPI
@@ -13,7 +13,7 @@ from datetime import datetime
 app = FastAPI()
 
 
-class Item(BaseModel):
+class User(BaseModel):
     name: Optional[str] = None
     gender: Optional[str] = None
     department: Optional[str] = None
@@ -30,13 +30,35 @@ def home():
     return {"Welcome in Employee Payroll App"}
 
 
-@app.post("/create-item/{item_id}")
-def create_item(item_id: int, item: Item):
-    if item_id in inventory:
+@app.post("/create-user/{user_id}")
+def create_user(user_id: int, user: User):
+    if user_id in inventory:
         return {"Error": "Item Id already exists "}
-    inventory[item_id] = {"Name": item.name, "Gender": item.gender, "Department": item.department,
-                          "Salary": item.salary, "Start_Date": item.start_date, "Notes": item.notes}
-    return {f"User created with name as {item.name}"}
+    inventory[user_id] = {"Name": user.name, "Gender": user.gender, "Department": user.department,
+                          "Salary": user.salary, "Start_Date": user.start_date, "Notes": user.notes}
+    return {f"User created with name as {user.name}"}
 
 
+@app.get("/dashboard")
+def dashboard():
+    return inventory
 
+
+@app.put("/update-item/{user_id}")
+def update_user(user_id: int, user: User):
+    if user_id not in inventory:
+        return {"Error": "Not Found"}
+    if user.name is not None:
+        inventory[user_id]["Name"] = user.name
+    if user.gender is not None:
+        inventory[user_id]["Gender"] = user.gender
+    if user.department is not None:
+        inventory[user_id]["Department"] = user.department
+    if user.salary is not None:
+        inventory[user_id]["Salary"] = user.salary
+    if user.start_date is not None:
+        inventory[user_id]["Start_Date"] = user.start_date
+    if user.notes is not None:
+        inventory[user_id]["Notes"] = user.notes
+
+    return {f"User update with name as {user.name}"}
